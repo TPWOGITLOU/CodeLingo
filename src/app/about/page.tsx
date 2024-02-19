50c2333f330fd5735f793bc2d8b35f518ddd5238
WolfieKnee <david@davidwolfson.co.uk>
2024-02-19 10:04:52 +0000

    add initial next project

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const About= ()  : JSX.Element =>{
+  return (
+    <section>
+      <h1>about</h1>
+      <Link href="/">home</Link>
+      </section>
+  );
+}
+
+export default About
