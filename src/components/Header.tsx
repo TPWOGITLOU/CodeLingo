048c7ea15fffb39f3905a37236b56f624d2e6bc8
Jessica Harper <jessicaharper@Jessicas-MacBook-Air.local>
2024-02-20 09:33:44 +0000

    add sprites to landing page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,28 @@
-import Link from "next/link"
-import ToggleTheme from "./ToggleTheme"
-const Header = () : JSX.Element => {
-    return (<>
-    <header className="flex flex-row justify-center gap-x-2">
-        <h1>CodeLingo</h1>
-        <Link className="" href="/about">About</Link>
-        <ToggleTheme />
-      </header>
-    </>)
-}
+import Link from "next/link";
+import ToggleTheme from "./ToggleTheme";
 
-export default Header
+const Header = (): JSX.Element => {
+  return (
+    <>
+      <header className="fixed flex flex-col text-xl align-middle">
+        <div className="flex flex-row contents-center">
+          <h1 className="font-bold text-3xl light-purple">CodeLingo</h1>
+          <div className="self-end">
+            <ToggleTheme />
+          </div>
+        </div>
+        <Link className="pt-5" href="/about">
+          About
+        </Link>
+        <Link className="pt-5" href="/topics">
+          Topics
+        </Link>
+        <Link className="pt-5" href="/progress">
+          Progress
+        </Link>
+      </header>
+    </>
+  );
+};
+
+export default Header;
