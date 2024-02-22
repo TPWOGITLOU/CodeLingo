92e41192e76a8f086f6901511dcf6cf5dd410b10
Jack Kent <jack.kent498@gmail.com>
2024-02-22 17:25:23 +0000

    basic challenge page

---
+++ src/app/topics/[language]/challenges/page.tsx
@@ -0,0 +1,51 @@
+import Header from "@/components/Header";
+import Link from "next/link"
+import TopicTile from "@/components/TopicTile";
+
+import { getQuestions } from "../../../../../lib/mongo/utils";
+
+const fetchQuestions = async (language: string)=>{
+    let collection:string;
+    switch (language) {
+        case "javascript":
+            collection="JS-Topics"
+            break;
+        case "python":
+            collection="PY-Topics"
+        break;
+        default:
+            collection="";
+            break;
+    }
+    try{
+        return await getQuestions(collection);
+    }catch(err){
+        throw new Error("There was a problem fetching the data in the topics page");
+    }
+}
+
+const Challanges = async ({params}: {params:{language:string}}) => {
+    const language = params.language;
+    const questionList = await fetchQuestions(language)
+
+return (<section>
+        <Header />
+        <div className="h-screen flex flex-col items-center justify-center content-center font-bold text-center font-mono">
+            <Link href="/">home</Link>
+            <h1 className="p-2 text-xl">Challenges</h1>
+            <h2>Here are the challenges for {language}</h2>
+            <div>
+                <ul>
+
+                {!questionList? <p>loading</p>: questionList.map((question: any)  => (
+                    <li key={question._id}>
+                        <p>hi</p>
+                    </li>
+                    ))}
+                </ul>
+            </div>           
+        </div>
+    </section>)
+}
+
+export default Challanges
