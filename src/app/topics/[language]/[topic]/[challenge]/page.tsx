8e24229ed754bf45053f60d6183ebba0f48ba815
jack-204 <117015021+jack-204@users.noreply.github.com>
2024-02-27 16:47:29 +0000

    removed redundant code used for testing

--- src/app/topics/[language]/[topic]/[challenge]/page.tsx
+++ src/app/topics/[language]/[topic]/[challenge]/page.tsx
@@ -27,25 +27,13 @@ const fetchChallenge = async (language: string, challenge_id: string) => {
   }
 }
 
-const mockChallenge : challenge = {
-    _id: '0',
-    topic: 'print',
-    language: 'javascript',
-    challengeType: 'block',
-    challengeQuestion:  'spell console log',
-    challengeSnippets: [],
-    answer:'',
-}
-
 
 const Challenge = async ({params}: {params:{language:string, topic:string, challenge:string}}) => {
     const language = params.language;
     const challenge_id = params.challenge;
     const result = await fetchChallenge(language, challenge_id)
-    //const result = mockChallenge
     if (result){
         const challengeData = result;
-        //challengeData.challengeType = "block"  //hard coded to block!
         switch (challengeData.challengeType) {
         case "multiChoice":
             return <MultipleChoice {...challengeData}/>
