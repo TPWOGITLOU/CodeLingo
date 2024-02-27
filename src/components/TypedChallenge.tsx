9c155969fc67240426ffe754fd2017493cab929c
WolfieKnee <david@davidwolfson.co.uk>
2024-02-27 18:49:23 +0000

    update styling for consistency with other components

--- src/components/TypedChallenge.tsx
+++ src/components/TypedChallenge.tsx
@@ -86,75 +86,73 @@ const TypedChallenge = (challengeData: challenge) => {
   };
 
   return (
-    <>
-      <div className="pt-5 h-full w-[80%] ml-[10%] grid grid-cols-4 grid-rows-4 gap-5">
-        <div className="col-start-1 col-end-3 row-span-full">
-          <Card className="h-[97.7%] bg-nice-yellow bg-opacity-50 border-8 border-border-colour">
-            <CardHeader className="flex gap-3">
-              <div className="flex flex-col">
-                <p className="text-3xl font-bold">Typing Challenge</p>
-              </div>
-            </CardHeader>
-            <CardBody>
-              {" "}
-              <p className="text-xl">Time to start typing....</p>
-            </CardBody>
-            <div className="bg-black">
-              <CodeEditor
-                code={code}
-                onChange={onChange}
-                language={language}
-                theme={theme}
-                height="40vh"
-              />
-            </div>
-          </Card>
+    <section id="component-container"
+    className=" 
+  w-[80%]
+  min-w-[450px]
+  mt-10 ml-auto mr-auto
+  box-border
+  ">
+    <div id="grid" className="grid grid-cols-2 grid-rows-2 gap-5 mb-5">
+      <Card id="terminal" className="
+      col-start-1 row-span-2 h-full 
+    bg-nice-yellow bg-opacity-50 
+      border-8 border-border-colour">
+        <CardHeader>
+          <p className="text-3xl font-bold">{challengeData.language} - {challengeData.topic}</p>
+        </CardHeader>
+        <CardBody>
+          <p className="text-xl">Time to start typing....</p>
+        </CardBody>
+        <div id="code-card-container"className="bg-black">
+          <CodeEditor 
+            code={code}
+            onChange={onChange}
+            language={language}
+            theme={theme}
+            height="400px"
+            />
         </div>
-        <div className="h-[34vh] col-start-3  col-span-3 row-start-3 row-span-2 mb-5">
-          <Card className="h-[99.8%] gap-3 p-5 border-8 border-border-colour bg-nice-yellow bg-opacity-50">
-            <OutputWindow outputDetails={outputDetails} feedback={feedback} />
-            <div className="flex flex-row justify-between">
-              <Button
-                onClick={fetchHandleCompile}
-                disabled={!code}
-                radius="full"
-                className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
-              >
-                {processing ? "Loading ..." : "Run"}
-              </Button>
-              <Button
-                onClick={checkAnswer}
-                disabled={!code}
-                radius="full"
-                className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
-              >
-                Check Answer
-              </Button>
-            </div>
-          </Card>
-        </div>
-        <div className="row-start-1 row-span-2 col-start-3 col-span-3">
-          <Card className="h-[100%] w-[100%] border-8 border-border-colour bg-nice-yellow bg-opacity-50  relative">
-            <CardBody className=" p-5 h-[100%] w-[80%] relative">
+        </Card>
+        <Card className="
+        border-8 border-border-colour 
+        bg-nice-yellow bg-opacity-50">
+          <CardBody className="p-5">
               <p className="text-xl">
-                <br /> <br />
                 {challengeData.challengeQuestion}
               </p>
-            </CardBody>
-            <div className="absolute right-5 bottom-5">
               <Image
                 src="/character2.png"
-                alt="2d pixel chara cter with her arms in the air"
+                alt="2d pixel character with her arms in the air"
                 className="p-0 h-[100%]"
               />
+            </CardBody>
+          </Card>
+        <div id="output" className="">
+          <Card className="h-full p-5 
+          border-8 border-border-colour bg-nice-yellow bg-opacity-50">
+          <OutputWindow outputDetails={outputDetails} feedback={feedback}/>
+          <div id="buttons" className="mt-3 flex flex-row justify-between">
+            <Button
+              onClick={fetchHandleCompile}
+              disabled={!code}
+              radius="full"
+              className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">
+              {processing ? "Loading ..." : "Run"}
+            </Button>
+            <Button
+              onClick={checkAnswer}
+              disabled={!code}
+              radius="full"
+              className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">
+                Check Answer
+            </Button>
             </div>
           </Card>
         </div>
       </div>
-      <div className="ml-[10%] mr-[10%]">
         <ChallengeFooter />
-      </div>
-    </>
+    </ section>
   );
 };
 
