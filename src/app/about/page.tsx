1d573dcbaecb5278b429dabef569d95a52358d05
WolfieKnee <david@davidwolfson.co.uk>
2024-02-27 15:37:26 +0000

    remove imports

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,3 @@
-import Link from "next/link";
-import MultipleChoice from "@/components/MultipleChoice";
-import Header from "@/components/Header";
 import TypedChallenge from "@/components/TypedChallenge";
 
 const About = (): JSX.Element => {
