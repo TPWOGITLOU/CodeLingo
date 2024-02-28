a159983f5efb488f15158ed9df4bbff6a9559aa3
WolfieKnee <david@davidwolfson.co.uk>
2024-02-28 11:17:25 +0000

    styling landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,27 @@ import Link from "next/link";
 
 const Home = (): JSX.Element => {
   return (
-    <main className="h-screen">
-      <div className="h-[90%] pt-10 flex flex-col items-center justify-center content-center font-bold text-center font-mono">
-        <div className="text-3xl pb-8">
+    <main className="h-[100%]">
+      <div className="font-bold text-center font-mono 
+      mt-[20px]
+      ">
+        <div id="welcome-text"className="text-3xl pb-8">
           <h2>Welcome to CodeLingo!</h2>
           <br />
           <p>Which language would you like to learn?</p>
         </div>
-        <div className="flex md:flex-row flex-col justify-between p-2 m-2">
-          <Link href="/python">
-            <div className="p-2 mb-2 flex flex-col items-center border-8 rounded-medium border-coral bg-nice-yellow bg-opacity-50 md:mr-3">
-              <h3 className="mb-5 p-5 pt-1 text-center text-lg dark:bg-black  bg-white rounded-lg">
+        <div id="sprite container" className="
+        flex flex-row flex-wrap
+        justify-center 
+        gap-5
+        ">
+          <Link href="/python" className="
+            p-5 
+            border-8 rounded-medium border-border-colour bg-nice-yellow bg-opacity-50
+            ">
+              <h3 className="
+                mb-5 px-5 py-2 
+                text-center text-2xl dark:bg-black  bg-white rounded-lg">
                 Python
               </h3>
               <SpriteAnimator
@@ -31,11 +41,13 @@ const Home = (): JSX.Element => {
                 animationSpeed={200}
                 imageSrc="/snake.png"
               />
-            </div>
           </Link>
-          <Link href="/javascript">
-            <div className="p-2 flex flex-col items-center border-8 rounded-medium border-coral bg-nice-yellow bg-opacity-50">
-              <h3 className="mb-5 p-5 pt-1 text-center text-lg  dark:bg-black bg-white rounded-lg">
+          <Link href="/javascript" className="
+            p-5 
+            border-8 rounded-medium border-border-colour bg-nice-yellow bg-opacity-50
+            ">
+              <h3 className="mb-5 px-5 py-2 
+                text-center text-2xl dark:bg-black  bg-white rounded-lg">
                 JavaScript
               </h3>
               <SpriteAnimator
@@ -51,7 +63,6 @@ const Home = (): JSX.Element => {
                 animationSpeed={200}
                 imageSrc="/coffeebean.png"
               />
-            </div>
           </Link>
         </div>
       </div>
